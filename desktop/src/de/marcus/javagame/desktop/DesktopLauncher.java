dcd88f0deb68d2412e955b92cbb30aff17c43ed0
MrCloudYT <realmrcloud@gmail.com>
2022-03-02 17:15:54 +0100

    Initial Commit

---
+++ desktop/src/de/marcus/javagame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.marcus.javagame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.marcus.javagame.JavaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JavaGame(), config);
+	}
+}
