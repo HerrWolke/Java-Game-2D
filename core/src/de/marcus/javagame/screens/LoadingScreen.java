51930485c53d2a81622d5adfc1e8ee3c6b075c54
rkali <ghostthe063@gmail.com>
2022-08-11 00:05:44 +0200

    Screen Manager

--- core/src/de/marcus/javagame/screens/LoadingScreen.java
+++ core/src/de/marcus/javagame/screens/LoadingScreen.java
@@ -44,7 +44,7 @@ public class LoadingScreen extends Game {
 
         //nur ein test
         if(loaded){
-            Timer t = new Timer();
+
             Gdx.gl.glClearColor( 0, 0, 0, 1 );
             Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
             g.setScreen(GameScreenManager.SCREENS.START_MENU);
--- core/src/de/marcus/javagame/screens/StartMenuScreen.java
+++ core/src/de/marcus/javagame/screens/StartMenuScreen.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -57,6 +59,16 @@ public class StartMenuScreen extends AbstractScreen{
         start = new ImageButton(startStyle);
         settings = new ImageButton(menuStyle);
         quit = new ImageButton(quitStyle);
+        //listener für startButton
+        start.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.gl.glClearColor( 0, 0, 0, 1 );
+                Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+                dispose();
+               app.g.setScreen(GameScreenManager.SCREENS.SELECT_PROFILE);
+            };
+        });
        //Bild für linken Rand
         leftImage = new Image(new Texture(Gdx.files.internal("items.png")));
         //vertical Group wo die Buttons reinkommen
