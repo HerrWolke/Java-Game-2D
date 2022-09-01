8e3496ffc7be09a5b6b1e233c64a138e8afe81d0
rkali <ghostthe063@gmail.com>
2022-09-01 22:40:18 +0200

    Screen Manager

--- core/src/de/marcus/javagame/screens/LoadingScreen.java
+++ core/src/de/marcus/javagame/screens/LoadingScreen.java
@@ -25,9 +25,7 @@ public class LoadingScreen extends Game {
 
         batch = new SpriteBatch();
         g = new GameScreenManager(this);
-        atlas = new TextureAtlas("running.atlas");
-        loadingAnimation =
-                new Animation<TextureRegion>(0.033f, atlas.findRegions("running"), Animation.PlayMode.LOOP);
+
         loaded = true;
 
     }
@@ -35,21 +33,19 @@ public class LoadingScreen extends Game {
     @Override
     public void render() {
 
-        TextureRegion frame = loadingAnimation.getKeyFrame(0.5f, true);
+
         //ScreenUtils.clear(1, 0, 0, 1);
-        batch.begin();
-        batch.draw(frame, 0, 0, 500, 500);
-
-        batch.end();
-
-        //nur ein test
         if (loaded) {
 
             Gdx.gl.glClearColor(0, 0, 0, 1);
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
             g.setScreen(GameScreenManager.SCREENS.START_MENU);
-
+            loaded =false;
+            dispose();
         }
+
+        //nur ein test
+
 
     }
 
