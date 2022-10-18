d9622fe186807afa35e1b503fa591f0a07036313
rkali <ghostthe063@gmail.com>
2022-10-18 16:15:47 +0200

    Screen Manager

--- core/src/de/marcus/javagame/world/GameWorld.java
+++ core/src/de/marcus/javagame/world/GameWorld.java
@@ -2,6 +2,7 @@ package de.marcus.javagame.world;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.PolygonMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -47,6 +48,26 @@ public class GameWorld {
         renderer = new OrthogonalTiledMapRenderer(dungeonRechts, UNIT_SCALE);
         renderer.setView(camera);
         renderer.setMap(tiledMap);
+        MapLayer collisionObjectLayer = tiledMap.getLayers().get("Nicht Betretbar");
+        MapObjects objects = collisionObjectLayer.getObjects();
+        for(PolygonMapObject mapobject : objects.getByType(PolygonMapObject.class)){
+            Polygon p = mapobject.getPolygon(); //
+            PolygonShape gb = new PolygonShape();
+            BodyDef bodydef = new BodyDef();
+            bodydef.type =  StaticBody;
+            Body bod = world.createBody(bodydef);
+
+            bodydef.position.set(new Vector2(0, 10));
+            float[] vertices = p.getVertices();
+
+            for (int id = 0; id < vertices.length; id += 2) {
+                vertices[id]   = (p.getX() + vertices[id])   ;
+                vertices[id+1] = (p.getY() + vertices[id+1]) ;
+            }
+
+            gb.set(vertices);
+            bod.createFixture(gb,0.0f);
+        }
     }
 
     public void render(OrthographicCamera camera) {
@@ -58,26 +79,7 @@ public class GameWorld {
         if (i == 0) {
             if (screen != i) {
                 renderer.setMap(tiledMap);
-                TiledMapTileLayer collisionObjectLayer = (TiledMapTileLayer)tiledMap.getLayers().get("Nicht Betretbar");
-                MapObjects objects = collisionObjectLayer.getObjects();
-                for(PolygonMapObject mapobject : objects.getByType(PolygonMapObject.class)){
-                    Polygon p = mapobject.getPolygon(); //
-                    PolygonShape gb = new PolygonShape();
-                    BodyDef bodydef = new BodyDef();
-                    bodydef.type = StaticBody;
-                    Body bod = world.createBody(bodydef);
 
-                    bodydef.position.set(new Vector2(0, 10));
-                    float[] vertices = p.getVertices();
-
-                    for (int id = 0; id < vertices.length; id += 2) {
-                        vertices[id]   = (p.getX() + vertices[id])   * 0.01f;
-                        vertices[id+1] = (p.getY() + vertices[id+1]) * 0.01f;
-                    }
-
-                    gb.set(vertices);
-                    bod.createFixture(gb,0.0f);
-                }
 
             }
         } else if (i == 1) {
