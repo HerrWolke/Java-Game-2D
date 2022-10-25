ada6001d527642e04a3a891442145704ef765142
MrCloudYT <realmrcloud@gmail.com>
2022-10-26 01:28:13 +0200

    Floaty

--- core/src/de/marcus/javagame/entities/NPC.java
+++ core/src/de/marcus/javagame/entities/NPC.java
@@ -8,7 +8,7 @@ import de.marcus.javagame.handlers.DialogHandler;
 import java.util.List;
 
 public class NPC extends Creature{
-    public NPC(DialogHandler.Dialogs dialog) {
+    public NPC(float x, float y,DialogHandler.Dialogs dialog) {
         super();
     }
 
